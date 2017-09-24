e99a71435a975c7258cd9f8d85ded0939b4ad4f3
Kal Academy <info@kalacademy.org>
2017-09-24 11:49:25 -0700

    Initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This is about a bank account
+    /// </summary>
+    class Account
+    {
+        private static int lastAccountNumber = 0;
+
+        #region Properties
+        /// <summary>
+        /// This holds the account number
+        /// </summary>
+        public int AccountNumber { get;  }
+
+        /// <summary>
+        /// This is the email address of the user
+        /// </summary>
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; private set; }
+        public string AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+
+
+        #region Constructors
+
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount; //Balance = Balance + amount
+            return Balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantitate an object
+            var account = new Account();
+            //account.AccountNumber = 1234;
+            account.EmailAddress = "test@test.com";
+            account.AccountType = "Savings";
+            //account.Balance = 1000000000000000;
+
+            var newBalance = account.Deposit(100);
+
+            Console.WriteLine($"AN: {account.AccountNumber}, EA: {account.EmailAddress}, Balance: {account.Balance:C}, AT: {account.AccountType}");
+
+            var account2 = new Account();
+            Console.WriteLine($"AN: {account2.AccountNumber}, EA: {account2.EmailAddress}, Balance: {account2.Balance:C}, AT: {account2.AccountType}");
+
+        }
+    }
+}
